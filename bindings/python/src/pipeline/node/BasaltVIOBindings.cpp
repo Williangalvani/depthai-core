91ae3f85fc1d634a3dda3c470dbd07216f1093f2
Willian Galvani <williangalvani@gmail.com>
2025-07-10 22:40:37 -0300

    more

--- bindings/python/src/pipeline/node/BasaltVIOBindings.cpp
+++ bindings/python/src/pipeline/node/BasaltVIOBindings.cpp
@@ -104,7 +104,7 @@ void bind_basaltnode(pybind11::module& m, void* pCallstack) {
         .def_readwrite("mapper_use_factors", &basalt::VioConfig::mapper_use_factors)
         .def_readwrite("mapper_use_lm", &basalt::VioConfig::mapper_use_lm)
         .def_readwrite("mapper_lm_lambda_min", &basalt::VioConfig::mapper_lm_lambda_min)
-        .def_readwrite("mapper_lm_lambda_max", &basalt::VioConfig::mapper_lm_lambda_max);
-        .def_readonly("quality", &BasaltVIO::quality, DOC(dai, node, BasaltVIO, quality))
+        .def_readwrite("mapper_lm_lambda_max", &basalt::VioConfig::mapper_lm_lambda_max)
+        .def_readonly("quality", &BasaltVIO::quality, DOC(dai, node, BasaltVIO, quality));
     ///////////////////////////////////////////////////////////////////////
 }
--- src/pipeline/datatype/StreamMessageParser.cpp
+++ src/pipeline/datatype/StreamMessageParser.cpp
@@ -41,6 +41,7 @@
 #include "depthai/pipeline/datatype/TrackedFeatures.hpp"
 #include "depthai/pipeline/datatype/Tracklets.hpp"
 #include "depthai/pipeline/datatype/TransformData.hpp"
+#include "depthai/pipeline/datatype/VIOQualityData.hpp"
 // shared
 #include "depthai/pipeline/datatype/DatatypeEnum.hpp"
 #include "depthai/utility/Serialization.hpp"
@@ -249,6 +250,9 @@ std::shared_ptr<ADatatype> StreamMessageParser::parseMessage(streamPacketDesc_t*
         case DatatypeEnum::ObjectTrackerConfig: {
             return parseDatatype<ObjectTrackerConfig>(metadataStart, serializedObjectSize, data, fd);
         }
+        case DatatypeEnum::VIOQualityData:
+            return parseDatatype<VIOQualityData>(metadataStart, serializedObjectSize, data, fd);
+            break;
     }
 
     throw std::runtime_error("Bad packet, couldn't parse");
